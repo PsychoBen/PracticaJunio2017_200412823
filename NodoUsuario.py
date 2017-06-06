c02e6202f0e8849ae3b0f7ea1399be6f8910a5f9
PsychoBen <esdrasbenjamin@gmail.com>
2017-06-06 03:21:35 -0600

    Add files via upload

---
+++ NodoUsuario.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        Clase Nodo Usuario
+# Purpose:
+#
+# Author:      Ben Cotto
+#
+# Created:     05/06/2017
+# Copyright:   (c) Ben Cotto
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+class NodoUsuario:
+
+    def __init__(self, nombreUsuario, passUsuario):
+        self.nombreUsuario = nombreUsuario
+        self.passUsuario = passUsuario
+        self.siguiente = None
+        self.anterior = None
+
+    def verNodoUsuario(self):
+        return {"NameUsuario": self.nombreUsuario, "Contrasenia": str(self.Contrasenia)}
