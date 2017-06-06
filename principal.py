afad392fe0380b1c2735582d2e9a2e7e04544a14
PsychoBen <esdrasbenjamin@yahoo.com>
2017-06-06 03:43:30 -0600

    Changes

---
+++ principal.py
@@ -0,0 +1,33 @@
+#-------------------------------------------------------------------------------
+# Name:        Modulo principal
+# Purpose:
+#
+# Author:      Ben Cotto
+#
+# Created:     05/06/2017
+# Copyright:   (c) Ben Cotto
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+import cmd
+import copy
+import NodoUsuario
+import ListaDobleCircularUsuario
+
+
+def main():
+    miListaUsuarios = ListaDobleCircularUsuario.ListaDobleCircularUsuario()
+    miListaUsuarios.crearCarpeta()
+    miListaUsuarios.insertarUsuarioFinal("a1","pass1")
+    miListaUsuarios.insertarUsuarioFinal("a1","pass1")
+    miListaUsuarios.insertarUsuarioFinal("a1","pass1")
+    miListaUsuarios.insertarUsuarioFinal("a2","pass1")
+    miListaUsuarios.insertarUsuarioFinal("a3","pass1")
+    miListaUsuarios.insertarUsuarioFinal("a3","pass1")
+    miListaUsuarios.insertarUsuarioFinal("a4","pass1")
+    longi = miListaUsuarios.obtenerLongitud()
+    print longi
+
+
+if __name__ == '__main__':
+    main()
